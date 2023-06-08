43d7d1fe66b0ae5bea66e76d444b911807bfbad4
ekhooley92 <ekhooley92@gmail.com>
2023-06-08 19:14:42 -0400

    Initial Commit
    Initial program; no frills. Outputs to the console.

---
+++ BMICalculator.js
@@ -0,0 +1,49 @@
+//BMI Calculator
+
+var HeightInches      = "60";
+var WeightPounds      = "140";
+var HeightCentimeters = 150;
+var WeightKilos       = 228;   
+var BMI               = 16;
+var WellnessLevel          = "Healthy"//Underweight, Healthy, Overweight, Obese
+
+HeightInches = prompt("Enter height in inches");
+
+HeightInches = Number(HeightInches);
+
+WeightPounds = prompt("Enter weight in pounds");
+
+WeightPounds = Number(WeightPounds);
+
+HeightCentimeters = HeightInches * 2.54;
+WeightKilos       = WeightPounds / 2.2046;
+
+BMI                = WeightKilos/((HeightCentimeters/100) * (HeightCentimeters/100));
+
+if(BMI < 18)
+{
+    WellnessLevel = "Underweight";
+}
+else if(BMI >= 18 && BMI <= 24)
+{
+    WellnessLevel = "Healthy";
+}
+else if(BMI > 24 && BMI <= 39)
+{
+    WellnessLevel = "Overweight";
+}
+else
+{
+    WellnessLevel = "Obese";
+}
+
+HeightInches = String(HeightInches);
+HeightCentimeters = String(HeightCentimeters);
+WeightPounds = String(WeightPounds);
+WeightKilos = String(WeightKilos);
+BMI = String(BMI);
+
+
+console.log(`The BMI of a person with a height of ${HeightInches} in (${HeightCentimeters} cm) and a weight of ${WeightPounds} lbs
+            (${WeightKilos} kg) is ${BMI}. This is considered ${WellnessLevel}. A BMI between 18 and 24 (inclusive) is considered
+            "healthy"`);
